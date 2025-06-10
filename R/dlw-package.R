0f1ebf4b121530326cfec52f2a2b57bb1cd2375c
R.Andres Castaneda <acastanedaa@worldbank.org>
2025-06-10 13:02:43 -0400

    packge doc

---
+++ R/dlw-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @rawNamespace import(collapse, except = fdroplevels)
+#' @rawNamespace import(data.table, except = fdroplevels)
+## usethis namespace: end
+NULL
+.datatable.aware = TRUE
--- R/dlw_server_catalog.R
+++ R/dlw_server_catalog.R
@@ -9,8 +9,19 @@
 #' \dontrun{
 #' dlw_server_catalog(sever = "GMD")
 #' }
-dlw_server_catalog <- function(server = NULL) {
+dlw_server_catalog <- function(server = NULL,
+                              dlw_url = NULL,
+                              api_version = getOption("dlw.default_api_version")
+                              ) {
   endpoint <- "ServerCatalog"
   base_server <- select_base_server(server = server)
 
+  req <- build_request(dlw_url = dlw_url,
+                       api_version = api_version,
+                       endpoint,
+                       Server = base_server)
+  req |>
+    httr2::req_perform() |>
+    httr2::resp_body_string() |>
+    fread()
 }

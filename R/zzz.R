031311d2eda974b89d24c2e2f95b7b7efb5795ee
R.Andres Castaneda <acastanedaa@worldbank.org>
2025-09-23 22:01:24 -0400

    only dta is working now

--- R/zzz.R
+++ R/zzz.R
@@ -6,7 +6,7 @@ op.dlw <- list(
   dlw.local_dir = Sys.getenv("DLW_local_dir"),
   dlw.board_type = c("folder", "local"),
   dlw.format     = c("qs", "parquet"),
-  dlw.download_formats = c("dta", "csv", "qs") # we could add more later.
+  dlw.download_formats = c("dta") # we could add more later.
 )
 
 
